775036432347d2755c624d8de573f40a6979f478
frectures <frectures@users.noreply.github.com>
2017-11-23 16:08:07 +0100

    ng generate component endstand

--- kicker-client/src/app/app.module.ts
+++ kicker-client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EndstandComponent } from './endstand/endstand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EndstandComponent
   ],
   imports: [
     BrowserModule
---
+++ kicker-client/src/app/endstand/endstand.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-endstand',
+  templateUrl: './endstand.component.html',
+  styleUrls: ['./endstand.component.css']
+})
+export class EndstandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
