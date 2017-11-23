bd2dfd9e8df1582a591aebedc8b37897cbdb76ee
frectures <frectures@users.noreply.github.com>
2017-11-23 16:28:14 +0100

    dynamic dummy table

--- kicker-client/src/app/endstand/endstand.component.ts
+++ kicker-client/src/app/endstand/endstand.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Endstand } from '../shared/endstand';
+
 @Component({
   selector: 'app-endstand',
   templateUrl: './endstand.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EndstandComponent implements OnInit {
 
+  endstaende: Endstand[] = [
+    { spieler1: "e", spieler2: "f", tore1: 3, tore2: 6 },
+    { spieler1: "g", spieler2: "h", tore1: 4, tore2: 6 },
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
