9f1d9a5e8a2ff0045c0934ab93db47ec6f81d47d
frectures <frectures@users.noreply.github.com>
2017-11-23 18:01:13 +0100

    log valid form data

--- kicker-client/src/app/app.module.ts
+++ kicker-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EndstandComponent } from './endstand/endstand.component';
@@ -15,7 +16,8 @@ import { NewEndstandComponent } from './new-endstand/new-endstand.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [EndstandService],
   bootstrap: [AppComponent]
